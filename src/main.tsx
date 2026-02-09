5ffb391fd598ff44279f6864ac3a35b84a015349
Daneris Mendoza <danerismendoza096@gmail.com>
2026-02-09 16:01:34 +0800

    toas functionality added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryProvider } from "./app/providers/QueryProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryProvider>
       <App />
+      <ToastContainer />
     </QueryProvider>
   </React.StrictMode>
 );
--- src/modules/products/hooks/useProducts.ts
+++ src/modules/products/hooks/useProducts.ts
@@ -1,6 +1,7 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getProducts, createProduct,   updateProduct, deleteProduct, } from "../api/products.api";
+import { getProducts, createProduct, updateProduct, deleteProduct, } from "../api/products.api";
 import type { CreateProductPayload, UpdateProductPayload } from "../types/product.types";
+import { toast } from "react-toastify";
 
 export const useProducts = () => {
   const queryClient = useQueryClient();
@@ -11,30 +12,34 @@ export const useProducts = () => {
   });
 
   const createProductMutation = useMutation({
-    mutationFn: (payload: CreateProductPayload) =>
-      createProduct(payload),
+    mutationFn: (payload: CreateProductPayload) => createProduct(payload),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
+      toast.success("Product created successfully!");
     },
+    onError: () => toast.error("Failed to create product."),
   });
 
-  // UPDATE: update product by ID
   const updateProductMutation = useMutation({
     mutationFn: ({ id, payload }: { id: number; payload: UpdateProductPayload }) =>
       updateProduct(id, payload),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
+      toast.success("Product updated successfully!");
     },
+    onError: () => toast.error("Failed to update product."),
   });
 
-  // DELETE: remove product by ID
   const deleteProductMutation = useMutation({
     mutationFn: (id: number) => deleteProduct(id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
+      toast.success("Product deleted successfully!");
     },
+    onError: () => toast.error("Failed to delete product."),
   });
-  
+
+
   return {
     productsQuery,
     createProductMutation,
